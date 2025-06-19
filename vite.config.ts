e8aa774848adf20d08919684fc4791e8080aa24d
Anvesha Yadav <anveshay2006@gmail.com>
2025-06-19 20:16:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'VibeCoding-Project',
+  base: '/VibeCoding-Project/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
